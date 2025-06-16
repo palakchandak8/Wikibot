0ffadd8da579960b64a6a3137978469470c41798
Palak  Chandak <palak.chandak@somaiya.edu>
2025-06-16 21:59:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function sendMessage() {
   input.value = "";
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/chat", {
+    const response = await fetch("https://palakchandak.pythonanywhere.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: userMessage })
